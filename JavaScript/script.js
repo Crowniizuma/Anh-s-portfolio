71e590a063d488ca7abc152324e3dbcdd26eb3ab
Anh <92283978+Crowniizuma@users.noreply.github.com>
2022-01-29 21:47:15 +0900

    update 2022/1/29

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,9 +2,25 @@ var navlist = new Vue({
     el: '#navbar',
     data: {
       items: [
-        { text: 'About'},
-        { text: 'Work'},
-        { text: 'Contact' }
+        { text: 'About', link: ''},
+        { text: 'Work', link: ''},
+        { text: 'Contact', link: ''}
       ]
     }
   })
+
+function asyncSleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function print_text(print_into, text, delay) {
+    $(print_into).html("");
+    for (var char of text) {
+        await asyncSleep(delay);
+        $(print_into).append(char);
+    }
+}
+(async function init_hello(){
+  await asyncSleep(1000);
+  print_text("#hello", "Hello, I'm Anh", 100);
+})();
