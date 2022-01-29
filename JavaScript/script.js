e620eae85c4c1d0e8327734ae2b44ae332ed7725
Anh <92283978+Crowniizuma@users.noreply.github.com>
2022-01-29 17:32:31 +0900

    new change

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,10 @@
-let navbarItems = ["Home", "About", "Work", "Contact"],
-    ul = document.createElement("ul");
-
-    document.getElementById("navbar").appendChild(ul);
-    navbarItems.forEach(function (item) {
-        let li = document.createElement("li");
-        ul.appendChild(li);
-        let a = document.createElement("a");
-        li.appendChild(a);
-        a.innerHTML += item;
-        
-    })
+var navlist = new Vue({
+    el: '#navbar',
+    data: {
+      items: [
+        { text: 'About'},
+        { text: 'Work'},
+        { text: 'Contact' }
+      ]
+    }
+  })
