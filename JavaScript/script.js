b6336db2953e93a399db9f9993979b666c4cb608
Anh <92283978+Crowniizuma@users.noreply.github.com>
2022-02-03 21:25:06 +0900

    new update 2022/2/3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -24,3 +24,33 @@ async function print_text(print_into, text, delay) {
   await asyncSleep(1000);
   print_text("#hello", "Hello, I'm Anh", 100);
 })();
+
+function reveal() {
+  let reveals = document.querySelectorAll(".info");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if(elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+function skillRun() {
+  let skillRun = document.querySelectorAll(".skill");
+  let skillSet = ["html", "css", "JavaScript"];
+  for (let i = 0; i < skillRun.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = skillRun[i].getBoundingClientRect().top;
+    let elementVisible = 50;
+    if(elementTop < windowHeight - elementVisible) {
+      skillRun[i].classList.add(skillSet[i]);
+    } else {
+      skillRun[i].classList.remove(skillSet[i]);
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", skillRun);
